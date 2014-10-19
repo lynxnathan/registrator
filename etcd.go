d167324374421364edddb0373f943f4298441314
Nathan Ribeiro <lynxnathan@gmail.com>
2014-10-19 04:51:27 -0200

    Changed etcd registration to include special SERVICE_DOMAINS env along the endpoint

--- etcd.go
+++ etcd.go
@@ -4,7 +4,8 @@ import (
 	"net"
 	"net/url"
 	"strconv"
-
+	"strings"
+	"log"
 	"github.com/coreos/go-etcd/etcd"
 )
 
@@ -21,8 +22,28 @@ func NewEtcdRegistry(uri *url.URL) ServiceRegistry {
 	return &EtcdRegistry{client: etcd.NewClient(urls), path: uri.Path}
 }
 
+func (r *EtcdRegistry) registerdomains(service *Service) {
+	log.Println("Called registerdomains")
+	path := r.path + "/" + service.Name + "/" + service.ID + "/domains"
+	if val, prs := service.Attrs["domains"]; prs {
+		domains := strings.Split(val, ",")
+		if len(domains) > 0 {
+			log.Println("Domains:", domains)
+			r.client.SetDir(path, uint64(0))
+			for _, domain := range domains {
+				_, err := r.client.Set(path + "/" + domain, domain, uint64(0))
+				if err != nil {
+					log.Println("Unable to create domain:", err)
+				}
+			}
+		}
+	}
+}
+
 func (r *EtcdRegistry) Register(service *Service) error {
-	path := r.path + "/" + service.Name + "/" + service.ID
+	log.Println("register called for:", service.Name)
+	path := r.path + "/" + service.Name + "/" + service.ID + "/endpoint"
+	r.registerdomains(service)
 	port := strconv.Itoa(service.Port)
 	addr := net.JoinHostPort(service.IP, port)
 	_, err := r.client.Set(path, addr, uint64(0))
